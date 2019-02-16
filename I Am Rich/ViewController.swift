a6d1225ff287eabe391b54f465f8ecb8d2709dc9
mcnkuna <mpho@cobiinteractive.com>
2019-02-16 20:05:27 +0200

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by mcnkuna on 2019/02/16.
+//  Copyright © 2019 mcnkuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
